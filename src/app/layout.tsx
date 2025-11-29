a00837184113d23e13371dad99318a31a5696d6a
using6843@gmail.com <using6843@gmail.com>
2025-11-29 17:59:36 +0900

    feat: auth provider 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import "./globals.css";
+import AuthProvider from "@/components/AuthProvider";
 
 export default function RootLayout({
   children,
@@ -10,9 +11,11 @@ export default function RootLayout({
   return (
     <html lang="ko">
       <body className="flex flex-col h-screen overflow-hidden">
-        <Header />
-        <main className="flex-1 min-h-0 flex flex-col overflow-hidden">{children}</main>
-        <Footer />
+        <AuthProvider>
+          <Header />
+          <main className="flex-1 min-h-0 flex flex-col overflow-hidden">{children}</main>
+          <Footer />
+        </AuthProvider>
       </body>
     </html>
   );
